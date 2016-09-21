e67176d825989fdd4b9f09f92e733c0d00f483d1
mnaumoski <naumoski.marija@gmail.com>
2016-09-21 09:25:47 -0400

    done with coders bay

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # Week7practice
 Practice week 7 challenging tasks.
-1. Click the button using Firebase.
-1.2. Create a countdown button from 100 and its restart mate as well.
-2. Sign in sheet with more properties.
 
+1. Click the button using Firebase.
+
+1.2. Create a countdown button from 100 and its restart-mate as well.
+
+2. Sign in sheet with more properties.
 
 2.1.  Sign up sheet as a variant of the above mentioned sign-in sheet that will keep recent user info
 
-3. CodersBay exercise; prep for the bidding game
-4. 
+3. CodersBay exercise;
+
+4. Adding and removing connections and creating specifiv pathways to store data
---
+++ codersBay.js
@@ -0,0 +1,72 @@
+  // Initialize Firebase
+  var config = {
+      apiKey: "AIzaSyAozUBLknvseKg0Ika4lQvM7tYNIDQdMpo",
+      authDomain: "brain-4b37c.firebaseapp.com",
+      databaseURL: "https://brain-4b37c.firebaseio.com",
+      storageBucket: "brain-4b37c.appspot.com",
+      messagingSenderId: "804931057770"
+  };
+  firebase.initializeApp(config);
+
+  // reference for firebase
+  var database = firebase.database();
+  // initial variables
+  var initialBidder = "Nobody";
+  var initialBid = 0;
+
+  var highBidder = initialBidder;
+  var highPrice = initialBid;
+
+  // ======================================= = = ===============================
+  // get status
+  database.ref().on("value", function(status) {
+      //if firebase has stored values for highBidder and highPrice
+      if (status.child("highBidder").exists() && status.child("highPrice").exists()) {
+
+          //set the initial vars to highbudder to the stored ones
+          highBidder = status.val().highBidder;
+          highPrice = status.val().highPrice;
+          // these initial values need to be displayed in their divs
+          $("#highestBidder").html(status.val().highBidder);
+          $("#highestPrice").html("$" + status.val().highPrice);
+          // print to console
+          console.log(status.val().highBidder);
+          console.log(status.val().highPrice);
+      }
+      // keep the initial vars equal to initial values of zero and nobody
+      else{
+        $("#highestBidder").html(highBidder);
+        $("#highestPrice").html("$" + highPrice);
+        console.log('Current High Price: ' + " " + highBidder +" "+ highPrice);
+        console.log("-------------------------------------------------------");
+      }
+      // watch for errors
+    }, function (errorObject) {
+      console.log('The read failed ' + errorObject.code);
+  });
+
+  //when a user clicks the button
+  $("#submitBid").on("click", function() {
+      // get the input values
+      var bidderName = $("#bidderName").val().trim();
+      var bidderPrice = $("#bidderPrice").val().trim();
+      // print to console
+      console.log(bidderName);
+      console.log(bidderPrice);
+
+      if (bidderPrice > highPrice) {
+        alert("You are now the highest bidder!");
+        // save it to firebase
+        database.ref().set({
+          highBidder: bidderName,
+          highPrice: bidderPrice
+        });
+        console.log('New High Price: ' + " " + highBidder + " " + highPrice);
+        console.log("-------------------------------------------------------");
+
+      } else {
+        alert("Sorry that bid is too low. Try again!");
+      }
+
+      return false;
+  })
