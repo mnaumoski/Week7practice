e82318347144786fa6f0ae2533f17719b75a3e33
mnaumoski <naumoski.marija@gmail.com>
2016-09-21 18:04:31 -0400

    progress with employee js

--- timesheet.js
+++ timesheet.js
@@ -1,28 +1,29 @@
   // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyD_6_GUQmiN7r8pmN81wsM9Y-W9yZMKoec",
-    authDomain: "myfirstproject-b8495.firebaseapp.com",
-    databaseURL: "https://myfirstproject-b8495.firebaseio.com",
-    storageBucket: "myfirstproject-b8495.appspot.com",
-    messagingSenderId: "987361683260"
+      apiKey: "AIzaSyDZIBk1N1VLk4UQPoXZ_nQXBEJ7pj6xZTk",
+      authDomain: "employeedata-4a108.firebaseapp.com",
+      databaseURL: "https://employeedata-4a108.firebaseio.com",
+      storageBucket: "",
+      messagingSenderId: "818698221016"
   };
-
   firebase.initializeApp(config);
-// 2. Button for adding Employees
-$("#addEmployeeBtn").on("click", function() {
-  // grab inputs
+
+  var database = firebase.database();
+  // 2. Button for adding Employees
+  $("#addEmployeeBtn").on("click", function() {
+      // grab inputs
       var empName = $("#employeeNameInput").val().trim();
       var empRole = $("#roleInpute").val().trim();
       var empRate = $("#rateInput").val().trim();
       var empStart = moment($("#startInput").val().trim(), "DD/MM/YY").format("x");
 
       var newEmp = {
-                name: empName,
-                role: empRole,
-                start: empStart,
-                rate: empRate
+          name: empName,
+          role: empRole,
+          start: empStart,
+          rate: empRate
       }
-      employeeData.push(newEmp);// this is new. uploads data to database with push
+      database.ref().push(newEmp); // this is new. uploads data to database with push
 
       // print to console
       console.log(newEmp.name);
@@ -37,11 +38,23 @@ $("#addEmployeeBtn").on("click", function() {
       $("#roleInput").val("");
       $("#startInput").val("");
       $("#rateInput").val("");
-})
+
+      return false;
+  });
+  // create Firebase event for adding employee to the database and a row in the html when a user adds an entry
+  database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+      console.log(childSnapshot.val());
+      console.log(childSnapshot.val().name);
+
+      var empName = childSnapshot.val().name;
+      var empRole = childSnapshot.val().role;
+      var empStart = childSnapshot.val().start;
+      var empRate = childSnapshot.val().rate;
+
+      console.log(empName);
+      console.log(empRate);
+      console.log(empStart);
+      console.log(empRole);
 
 
-
-
-
-
-
+  })
